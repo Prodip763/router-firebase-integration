436db5721ac60d9a78e72e28c53b3e41718a7ff3
prodip763 <prodip12763@gmail.com>
2022-08-14 03:05:46 +0600

    firebase-hooks

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,18 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+// import useFirebase from "../../hooks/useFirebase";
+
+const auth = getAuth(app);
 
 const Home = () => {
+    // const {user} = useFirebase();
+    const [user] = useAuthState(auth);
     return(
         <div>
             <h2>This is Home</h2>
+            <p>Current user: {user? user.displayName : 'no body here'}</p>
         </div>
     );
 };
